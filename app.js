7ee7a1e33eafb8df8d31f6ace8fcc1d3a784db74
Cynthia Yeung <cyeung91@gmail.com>
2015-02-24 17:38:34 -0800

    Changed HTML text content and added alternating player turn text prompts

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", function () {
 var trackPlayer = "X"; 
+var counter = 1;
+var playerPrompt = document.querySelector(".playerTurn");
 
 var squares = document.getElementsByClassName("square");
 		for (var i = 0; i < squares.length; i++) {
@@ -7,19 +9,27 @@ var squares = document.getElementsByClassName("square");
 		}
 
 	function clickSquare() {
-	 	console.log("This square is clicked"); // ---> Check to see that square is being clicked.
-	 	// event.target.style.backgroundColor = "gray"; // --> When square is clicked, change square to gray.
-		
-		if (event.target.innerHTML === "") {
-			event.target.innerHTML = trackPlayer; // First condition is that event.target is defined as X
-		} 
-		if (trackPlayer === "X") {
-			trackPlayer = "O"; // Second condition, if player is X, the trackPlayer will switch to O player
-			event.target.style.backgroundColor = "gray"; // Also when clicked, turn background to gray with this value
-		} 
-		else {
-			trackPlayer = "X"; // When player is O, the trackPlayer will switch to X 
-			event.target.style.backgroundColor = "yellow"; // When O clicks, the background turns yellow
+		 if (event.target.innerHTML.length === 0){
+		 	console.log("This square is clicked"); // ---> Check to see that square is being clicked.
+		 	// event.target.style.backgroundColor = "gray"; // --> When square is clicked, change square to gray.
+			
+			if (event.target.innerHTML === "") {
+				event.target.innerHTML = trackPlayer; // First condition is that event.target is defined as X
+			
+				if (trackPlayer === "X") {
+					trackPlayer = "O"; // Second condition, if player is X, the trackPlayer will switch to O player
+					event.target.style.backgroundColor = "gray"; // Also when clicked, turn background to gray with this value
+					counter++;
+					playerPrompt.innerHTML = "Player O turn";
+
+				} 
+				else {
+					trackPlayer = "X"; // When player is O, the trackPlayer will switch to X 
+					event.target.style.backgroundColor = "yellow"; // When O clicks, the background turns yellow
+					counter++;
+					playerPrompt.innerHTML = "Player X turn";
+				}
+			}
 		}
 	}
 	
